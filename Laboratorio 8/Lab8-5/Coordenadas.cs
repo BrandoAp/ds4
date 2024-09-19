15796a49daa1a8fd594169d6d85e5a136398e6f5
BrandoAp <avilabrando14@gmail.com>
2024-09-18 20:51:40 -0500

    Terminal lab 8 y corregir lab 8.7

---
+++ Laboratorio 8/Lab8-5/Coordenadas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_5
+{
+    public partial class Coordenadas
+    {
+        private int x;
+        private int y; 
+
+        public Coordenadas(int x, int y) {
+            this.x = x; 
+            this.y = y;
+        }
+    }
+
+    public partial class Coordenadas 
+    { 
+        public void Vercoordenadas()
+        {
+            Console.WriteLine("Coordenadas: {0}, {1}", x, y);
+        }
+    
+    }
+}
---
+++ Laboratorio 8/Lab8-6/ClaseBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_6
+{
+    public class ClaseBase
+    {
+        public void test()
+        {
+
+        }
+        public virtual void masTest() //Se cambia la palabra sealed por virtual
+        {
+
+        }
+    }
+
+    class ClaseHijo : ClaseBase 
+    { 
+        public override void masTest()
+        {
+
+        }    
+    }
+}
