79ef01f45cfa9b97260cc77ed4029f5e6dab889c
BrandoAp <avilabrando14@gmail.com>
2024-09-25 18:38:40 -0500

    Laboratorio 10 Terminado

---
+++ Laboratorio 10/Lab10-3/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Peso
+    Sub Main(args As String())
+        Dim M As Double
+        Dim G As Double
+        Dim P As Double
+
+        G = 9.8
+        Console.Write("Ingrese la masa del objeto: ")
+        M = Console.ReadLine
+
+        P = M * G
+
+        Console.WriteLine("Peso del objeto: {0}", P)
+        Console.ReadKey()
+
+
+    End Sub
+End Module
